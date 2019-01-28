23090467a9c147c8805bf846eae2a83853ad3735
Nagarro <>
2019-01-28 19:07:18 +0530

    setup

---
+++ TabeedDemo/TabeedDemo/App.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Naxam.Controls.Forms;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace TabeedDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var tabs = new TopTabbedPage
+            {
+                Title = "TopTabs",
+                BarBackgroundColor = Color.FromHex("9C27B0"),
+                BarTextColor = Color.Black
+               // SwipeEnabled = false,
+                //BarIndicatorColor = Color.DeepPink,
+                //BarTextColor = Color.DeepPink
+            };
+
+            tabs.CurrentPageChanged += Tabs_CurrentPageChanged;
+            tabs.Children.Add(new Page1
+            {
+                Title = "My Page",
+                
+                BackgroundColor = Color.Aquamarine
+            });
+            tabs.Children.Add(new Page2
+            {
+                Title = "Tab 1",
+                BackgroundColor = Color.Aqua
+            });
+
+            var m = new NavigationPage(tabs)
+            {
+                BarBackgroundColor = Color.FromHex("9C27B0"),
+                BarTextColor = Color.White
+            };
+            m.PropertyChanged += (sender, e) =>
+            {
+                System.Diagnostics.Debug.WriteLine(e.PropertyName);
+            };
+
+            MainPage = m;
+        }
+
+        void Tabs_CurrentPageChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ TabeedDemo/TabeedDemo/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Naxam.Controls.Forms;
+using Xamarin.Forms;
+
+namespace TabeedDemo
+{
+    public partial class MainPage : TopTabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+           
+        }
+    }
+}
